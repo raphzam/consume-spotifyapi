55a5415a0eceaba70010ec3f6579460002bbb074
raphzam <60793109+raphzam@users.noreply.github.com>
2021-07-26 18:14:10 -0400

    Initial commit

---
+++ src/main/java/com/example/consumespotifyapi/ConsumeSpotifyApiApplication.java
@@ -0,0 +1,21 @@
+package com.example.consumespotifyapi;
+
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.specification.Track;
+import com.wrapper.spotify.requests.data.tracks.GetTrackRequest;
+import org.apache.hc.core5.http.ParseException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class ConsumeSpotifyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumeSpotifyApiApplication.class, args);
+    }
+
+}
