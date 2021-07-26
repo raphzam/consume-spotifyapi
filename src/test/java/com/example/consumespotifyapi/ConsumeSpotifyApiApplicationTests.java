3b322dadf4f0a6885ef68558e30ce60538818629
raphzam <60793109+raphzam@users.noreply.github.com>
2021-07-26 18:19:58 -0400

    Add files from upload

---
+++ src/test/java/com/example/consumespotifyapi/ConsumeSpotifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumespotifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumeSpotifyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
